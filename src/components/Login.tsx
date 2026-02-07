65fa7730361dd5fc60e9b24a95da494ce035238f
bobbygour30 <bobby30gour@gmail.com>
2026-02-07 16:24:16 +0530

    updated

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -46,7 +46,7 @@ export const Login = ({ onLoginSuccess }: LoginProps) => {
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-amber-500"
-            placeholder="admin@golden.com"
+            placeholder="Username"
             required
           />
         </div>
